3d42547f76f7534a9c6334ff1652ef9e7eeec377
Julio Cesar Zapata <juliozapata@MacBook-Pro-10.local>
2018-11-04 22:16:33 -0500

    test

--- app/middlewares/signup-validations.middleware.js
+++ app/middlewares/signup-validations.middleware.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/users', (req, res) => {
-  const user = req.body;
+  const name = req.body.name;
+  const lastName = req.body.name;
+  const email = req.body.name;
+  const password = req.body.name;
+
+  req.checkBody('name');
 
   // console.log(`preueba de que esta monda funciona aqui ${JSON.stringify(user)}`);
   res.send('test');
