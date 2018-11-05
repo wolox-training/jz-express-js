764892a807c7e2a575b26f1edb9ee8931531c40e
jxczz2 <julio.zapata@wolox.co>
2018-11-05 12:10:23 -0500

    it is added all snakecase in migrations

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,19 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false },
-    underscore: true,
-    freezeTableName: true
-  });
+  const User = sequelize.define(
+    'User',
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false }
+    },
+    {
+      underscore: true,
+      freezeTableName: true
+    }
+  );
 
   return User;
 };
