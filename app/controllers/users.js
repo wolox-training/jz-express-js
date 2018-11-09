27fb28f24b6bf24e4ad5d2f662f7d6445214d64b
jxczz2 <julio.zapata@wolox.co>
2018-11-09 16:42:31 -0500

    refactor controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const error = require('../errors'),
-  { validateUser } = require('../middlewares/validations'),
   User = require('../models').User;
 
 exports.userCreate = (req, res, next) => {
-  const user = req.body;
-  User.createUser(user)
+  User.createUser(req.body)
     .then(() => {
       res.status(201).end();
     })
