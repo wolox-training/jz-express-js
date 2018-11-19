f5448914736dc276f7288644e6f5a6c6f97b9c95
jxczz2 <julio.zapata@wolox.co>
2018-11-19 18:02:03 -0500

    router  of administrator user record is added

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,10 @@
-const signUpMiddleware = require('./middlewares/signUpValidations'),
+const signMiddleware = require('./middlewares/signValidations'),
   auth = require('./middlewares/auth'),
   userController = require('./controllers/users');
 
 exports.init = app => {
-  app.post('/users', signUpMiddleware.checkValidations, userController.userCreate);
+  app.post('/users', signMiddleware.checkValidations, userController.userCreate);
   app.post('/users/sessions', userController.session);
   app.get('/users/', auth.verifyToken, userController.userList);
-  app.post('/admin/users/');
+  app.post('/admin/users/', auth.verifyToken, auth.verifyPermission, userController.singUpAdmin);
 };
