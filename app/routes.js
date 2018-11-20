275634af2c47024e04bea99c76f7d0e9e74959ff
jxczz2 <julio.zapata@wolox.co>
2018-11-20 16:19:48 -0500

    Changes are added to the validation middleware sign

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,14 @@
-const signMiddleware = require('./middlewares/signValidations'),
+const {
+    validatorMiddleware,
+    signUpCheckValidations,
+    signInCheckValidations
+  } = require('./middlewares/signValidations'),
   auth = require('./middlewares/auth'),
   userController = require('./controllers/users');
 
 exports.init = app => {
-  app.post('/users', signMiddleware.checkValidations, userController.userCreate);
-  app.post('/users/sessions', userController.session);
+  app.post('/users', validatorMiddleware(signUpCheckValidations), userController.userCreate);
+  app.post('/users/sessions', validatorMiddleware(signInCheckValidations), userController.session);
   app.get('/users/', auth.verifyToken, userController.userList);
   app.post('/admin/users/', auth.verifyToken, auth.verifyPermission, userController.singUpAdmin);
 };
