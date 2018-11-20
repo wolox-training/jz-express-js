7eea8aee6f2826545d72b8ae0137ed9f9647680d
jxczz2 <julio.zapata@wolox.co>
2018-11-20 17:44:09 -0500

    Validation middleware is added in signUp of administrator user

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,20 @@
-const {
+const auth = require('./middlewares/auth'),
+  userController = require('./controllers/users'),
+  {
     validatorMiddleware,
     signUpCheckValidations,
     signInCheckValidations
-  } = require('./middlewares/signValidations'),
-  auth = require('./middlewares/auth'),
-  userController = require('./controllers/users');
+  } = require('./middlewares/signValidations');
 
 exports.init = app => {
   app.post('/users', validatorMiddleware(signUpCheckValidations), userController.userCreate);
   app.post('/users/sessions', validatorMiddleware(signInCheckValidations), userController.session);
   app.get('/users/', auth.verifyToken, userController.userList);
-  app.post('/admin/users/', auth.verifyToken, auth.verifyPermission, userController.singUpAdmin);
+  app.post(
+    '/admin/users/',
+    auth.verifyToken,
+    auth.verifyPermission,
+    validatorMiddleware(signUpCheckValidations),
+    userController.singUpAdmin
+  );
 };
