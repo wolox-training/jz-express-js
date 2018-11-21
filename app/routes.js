a8ac351bf14c7a47084e7d4c6ee8c1547694b394
jxczz2 <julio.zapata@wolox.co>
2018-11-21 16:17:58 -0500

    conflict merger is repaired

--- app/routes.js
+++ app/routes.js
@@ -4,12 +4,12 @@ const auth = require('./middlewares/auth'),
     validatorMiddleware,
     signUpCheckValidations,
     signInCheckValidations
-  } = require('./middlewares/signValidations'),
+  } = require('./middlewares/checkValidations'),
   verifyPermission = [auth.verifyToken, auth.verifyPermission, validatorMiddleware(signUpCheckValidations)];
 
 exports.init = app => {
   app.post('/users', validatorMiddleware(signUpCheckValidations), userController.userCreate);
-  app.post('/users/sessions', validatorMiddleware(signInCheckValidations), userController.session);
+  app.post('/users/sessions', validatorMiddleware(signInCheckValidations), userController.signIn);
   app.get('/users', auth.verifyToken, userController.userList);
   app.post('/admin/users', verifyPermission, userController.singUpAdmin);
 };
