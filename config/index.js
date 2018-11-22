d3871330610ac68f94a12c35352cf7de29c9022f
jxczz2 <julio.zapata@wolox.co>
2018-11-22 14:28:41 -0500

    albumsApi is added in config to set the url

--- config/index.js
+++ config/index.js
@@ -48,6 +48,9 @@ const config = {
     rollbar: {
       accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
       environment: process.env.ROLLBAR_ENV
+    },
+    albumsApi: {
+      url: process.env.API_URL || 'https://jsonplaceholder.typicode.com'
     }
   }
 };
