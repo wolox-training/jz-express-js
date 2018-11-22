6a5d8ae6edabc3709191a64e77d28127556ec24e
jxczz2 <julio.zapata@wolox.co>
2018-11-22 10:01:49 -0500

    its added the array directly in the route admin/users

--- app/routes.js
+++ app/routes.js
@@ -4,12 +4,15 @@ const auth = require('./middlewares/auth'),
     validatorMiddleware,
     signUpCheckValidations,
     signInCheckValidations
-  } = require('./middlewares/checkValidations'),
-  verifyPermission = [auth.verifyToken, auth.verifyPermission, validatorMiddleware(signUpCheckValidations)];
+  } = require('./middlewares/checkValidations');
 
 exports.init = app => {
   app.post('/users', validatorMiddleware(signUpCheckValidations), userController.userCreate);
   app.post('/users/sessions', validatorMiddleware(signInCheckValidations), userController.signIn);
   app.get('/users', auth.verifyToken, userController.userList);
-  app.post('/admin/users', verifyPermission, userController.singUpAdmin);
+  app.post(
+    '/admin/users',
+    [auth.verifyToken, auth.verifyPermission, validatorMiddleware(signUpCheckValidations)],
+    userController.singUpAdmin
+  );
 };
