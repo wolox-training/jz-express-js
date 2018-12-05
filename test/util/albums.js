97369687ef9b190f5938690dbf26e58f66850ec6
jxczz2 <julio.zapata@wolox.co>
2018-12-05 16:07:35 -0500

    mutation are added

--- test/util/albums.js
+++ test/util/albums.js
@@ -1,3 +1,5 @@
 'use strict';
 
-exports.queryAlbums = { query: `{  albums { title id } }`, variables: null };
+exports.queryAlbums = { query: `{  albums { title id } }` };
+exports.mutationAlbum = { query: `mutation {delete(id: 1)}` };
+exports.otherMutationAlbum = { query: `mutation {delete(id: 6)}` };
