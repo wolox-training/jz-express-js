08025df8b1b3b721e919751696d115003dff005c
jxczz2 <julio.zapata@wolox.co>
2018-12-14 11:54:14 -0500

    service to consume api training is added

---
+++ express-js-bootstrap-graphql/app/services/trainingApi.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const axios = require('axios'),
+  config = require('../../config'),
+  errors = require('../errors'),
+  logger = require('../logger'),
+  url = config.common.albumsApi.url;
+
+exports.getResources = source =>
+  axios
+    .get(`${url}${source}`)
+    .then(res => res.data)
+    .catch(err => {
+      logger.error(err);
+      if (err.response.status === 300)
+        throw errors.albumsApiRedirection('client must take additional action to complete the request');
+      if (err.response.status === 404) throw errors.albumsNotFound('Error 404 No found');
+
+      throw errors.albumsApiError('service not available');
+    });
+
+exports.postResources = (source, param) =>
+  axios
+    .post(`${url}${source}`, param)
+    .then(res => res.data)
+    .catch(err => {
+      logger.error(err);
+      if (err.response.status === 300)
+        throw errors.albumsApiRedirection('client must take additional action to complete the request');
+      if (err.response.status === 404) throw errors.albumsNotFound('Error 404 No found');
+
+      throw errors.albumsApiError('service not available');
+    });
