fcf2cc681a3ed48d4df795c19357663ad6bb36e1
jxczz2 <julio.zapata@wolox.co>
2018-12-16 09:19:47 -0500

    mutation create user is added

--- express-js-bootstrap-graphql/app/graphql/users/mutations.js
+++ express-js-bootstrap-graphql/app/graphql/users/mutations.js
@@ -1,8 +1,19 @@
-const {
-    GraphQLObjectType,
-    GraphQLNonNull,
-    GraphQLString,
-    GraphQLID,
-    GraphQLInt,
-    GraphQLInputObjectType
-  } = require('graphql');
+const { GraphQLNonNull, GraphQLBoolean } = require('graphql'),
+  { getResources, postResources } = require('../../services/trainingApi'),
+  { userInputType } = require('./types');
+
+exports.createUser = {
+  description: 'create a single user',
+  type: GraphQLBoolean,
+  args: {
+    data: {
+      name: 'data',
+      type: new GraphQLNonNull(userInputType)
+    }
+  },
+  resolve: async (obj, { data }, context, info) => {
+    const createUser = await postResources('/users', data);
+    console.log(`Aquii esta la creacion del usuario ${JSON.stringify(createUser)}`);
+    return true;
+  }
+};
