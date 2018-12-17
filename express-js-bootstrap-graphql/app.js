03b355b108a25324590a806e50666d80c1ac81d3
jxczz2 <julio.zapata@wolox.co>
2018-12-17 13:15:33 -0500

    migrations are removed

--- express-js-bootstrap-graphql/app.js
+++ express-js-bootstrap-graphql/app.js
@@ -5,7 +5,6 @@ const express = require('express'),
   cors = require('cors'),
   config = require('./config'),
   schema = require('./app/graphql'),
-  migrationsManager = require('./migrations'),
   logger = require('./app/logger'),
   DEFAULT_BODY_SIZE_LIMIT = 1024 * 1024 * 10,
   DEFAULT_PARAMETER_LIMIT = 10000;
@@ -42,11 +41,6 @@ const init = () => {
   }
 
   Promise.resolve()
-    .then(() => {
-      if (!config.isTesting) {
-        return migrationsManager.check();
-      }
-    })
     .then(() => {
       app.use(
         '/',
