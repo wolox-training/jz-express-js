14422bb992bf19b055ceb4460cb7f86777160823
jxczz2 <julio.zapata@wolox.co>
2018-12-20 14:33:30 -0500

    elements for test are added

---
+++ express-js-bootstrap-graphql/test/util/usersQueries.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const chai = require('chai'),
+  server = require('../../app');
+
+exports.userOne = {
+  name: 'sarahi',
+  lastName: 'torres',
+  email: 'sarahi12hg@wolox.com',
+  password: 'woloxwoloA1520'
+};
+
+exports.userWithBadPassword = {
+  name: 'sarahi',
+  lastName: 'torres',
+  email: 'sarahidamaris12hg@woloxi.com',
+  password: 'facilfacil'
+};
+
+exports.anotherUser = {
+  name: 'sarahi',
+  lastName: 'torres',
+  email: 'sarahidamaris12hg@wolox.com',
+  password: 'woloxwoloA1520'
+};
+
+exports.adminUser = {
+  name: 'damarisTorres',
+  lastName: 'palacios',
+  email: 'dami@wolox.com',
+  password: '$2y$10$2gFqkr3E8D6EGOc06WlbBOqlvLaVsDkNDxN68XXxM2iuLD8HZwD7S',
+  roleUser: 'administrator'
+};
+
+exports.createUser = user =>
+  chai
+    .request(server)
+    .post('/')
+    .send(user);
+
+exports.login = credentials =>
+  chai
+    .request(server)
+    .post('/')
+    .send(credentials);
