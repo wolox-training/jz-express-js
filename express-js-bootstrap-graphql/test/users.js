dfc2c2b56c75556b89a57b73e4dc5517a2ee28cf
jxczz2 <julio.zapata@wolox.co>
2018-12-20 14:57:38 -0500

    first test of the tests is added

--- express-js-bootstrap-graphql/test/users.js
+++ express-js-bootstrap-graphql/test/users.js
@@ -5,83 +5,94 @@ const chai = require('chai'),
   nock = require('nock'),
   server = require('./../app'),
   config = require('../config'),
-  { queryAlbums, mutationAlbumDelete, albumCreatedApi, mutationCreateAlbum } = require('./util/usersQueries'),
-  url = config.common.albumsApi.url,
+  {
+    createUser,
+    login,
+    mutationAlbumDelete,
+    albumCreatedApi,
+    mutationCreateAlbum,
+    userOne
+  } = require('./util/usersQueries'),
+  { resCreateUser } = require('./util/mockedResponseApi'),
+  url = config.common.trainingApi.url,
   expect = chai.expect;
 
 describe('grahphQlService ', () => {
-  describe('Delete album with graphql ', () => {
+  describe('test endpoints api with graphQl ', () => {
+    it('should created user with out problems', done => {
+      const [mockedApi] = createUser;
+
+      nock(`${url}/users`)
+        .get('')
+        .reply(201, mockedApi);
+      createUser(userOne).then(res => {
+        expect(res).have.status(201);
+        expect(res.body.data).to.equal('');
+        done();
+      });
+    });
+
     it('should deleted with graphql the album purchased without problems because user is logged and and he has the albums', done => {
-      const [mockedAlbum] = albums;
+      const [mockedAlbum] = createUser;
 
       nock(`${url}/1`)
         .get('')
         .reply(200, mockedAlbum);
 
-      User.create(adminUser).then(() => {
-        login({
-          email: 'dami@wolox.com',
-          password: 'woloxwoloA152022'
-        }).then(res => {
-          chai
-            .request(server)
-            .post('/albums/1')
-            .set(config.common.session.header_name, res.headers[config.common.session.header_name])
-            .send()
-            .then(() => {
-              chai
-                .request(server)
-                .post('/graph-albums')
-                .set(config.common.session.header_name, res.headers[config.common.session.header_name])
-                .send(mutationAlbumDelete(1))
-                .then(async result => {
-                  const album = await Album.find({
-                    where: {
-                      albumId: 1,
-                      userId: 1
-                    }
-                  });
-                  expect(result).have.status(200);
-                  expect(result.body.data.deleteAlbum).to.equal('Album  is deleted');
-                  expect(album).to.be.equal(null);
-                  done();
-                });
-            });
-        });
+      login({
+        email: 'dami@wolox.com',
+        password: 'woloxwoloA152022'
+      }).then(res => {
+        chai
+          .request(server)
+          .post('/albums/1')
+          .set(config.common.session.header_name, res.headers[config.common.session.header_name])
+          .send()
+          .then(() => {
+            chai
+              .request(server)
+              .post('/graph-albums')
+              .set(config.common.session.header_name, res.headers[config.common.session.header_name])
+              .send(mutationAlbumDelete(1))
+              .then(async result => {
+                expect(result).have.status(200);
+                expect(result.body.data.deleteAlbum).to.equal('Album  is deleted');
+
+                done();
+              });
+          });
       });
     });
 
     it('should fail delete with graphql the album because user does not have this album', done => {
-      const [mockedAlbum] = albums;
+      const [mockedAlbum] = createUser;
 
       nock(`${url}/1`)
         .get('')
         .reply(200, mockedAlbum);
 
-      User.create(adminUser).then(() => {
-        login({
-          email: 'dami@wolox.com',
-          password: 'woloxwoloA152022'
-        }).then(res => {
-          chai
-            .request(server)
-            .post('/albums/1')
-            .set(config.common.session.header_name, res.headers[config.common.session.header_name])
-            .send()
-            .then(() => {
-              chai
-                .request(server)
-                .post('/graph-albums')
-                .set(config.common.session.header_name, res.headers[config.common.session.header_name])
-                .send(mutationAlbumDelete(6))
-                .then(result => {
-                  expect(result).have.status(200);
-                  expect(result.body.errors[0].message).to.equal('Album not found');
-                  expect(result.body.errors[0].statusCode).to.equal(404);
-                  done();
-                });
-            });
-        });
+      login({
+        email: 'dami@wolox.com',
+        password: 'woloxwoloA152022'
+      }).then(res => {
+        chai
+          .request(server)
+          .post('/albums/1')
+          .set(config.common.session.header_name, res.headers[config.common.session.header_name])
+          .send()
+          .then(() => {
+            chai
+              .request(server)
+              .post('/graph-albums')
+              .set(config.common.session.header_name, res.headers[config.common.session.header_name])
+              .send(mutationAlbumDelete(6))
+              .then(result => {
+                expect(result).have.status(200);
+                expect(result.body.errors[0].message).to.equal('Album not found');
+                expect(result.body.errors[0].statusCode).to.equal(404);
+                done();
+              });
+          });
       });
     });
   });
@@ -93,21 +104,19 @@ describe('grahphQlService ', () => {
         .post('')
         .reply(200, albumCreatedApi);
 
-      User.create(adminUser).then(() => {
-        login({
-          email: 'dami@wolox.com',
-          password: 'woloxwoloA152022'
-        }).then(res => {
-          chai
-            .request(server)
-            .post('/graph-albums')
-            .set(config.common.session.header_name, res.headers[config.common.session.header_name])
-            .send(mutationCreateAlbum)
-            .then(result => {
-              expect(result).have.status(200);
-              done();
-            });
-        });
+      login({
+        email: 'dami@wolox.com',
+        password: 'woloxwoloA152022'
+      }).then(res => {
+        chai
+          .request(server)
+          .post('/graph-albums')
+          .set(config.common.session.header_name, res.headers[config.common.session.header_name])
+          .send(mutationCreateAlbum)
+          .then(result => {
+            expect(result).have.status(200);
+            done();
+          });
       });
     });
     it('should fail create album with graphql because user is not logged ', done => {
